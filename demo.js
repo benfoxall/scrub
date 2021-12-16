b65c70b85d8e293c6595564356d1d397c2ed0bda
Ben Foxall <benfoxall@gmail.com>
2021-12-16 10:49:06 +0000

    touch support

--- demo.js
+++ demo.js
@@ -33,32 +33,59 @@ function getFrameStats() {
     return "Extracted " + frameCount + " frames" + fps;
 }
 
-const frames = window.frames = []
+
 const bitmaps = window.bitmaps = []
 
-window.addEventListener('mousemove', (e) => {
-    const idx = Math.floor((e.clientX / window.innerWidth) * bitmaps.length)
-    const bitmap = bitmaps[idx]
+let idx = 0;
 
+function render() {
+    const bitmap = bitmaps[idx]
     if (bitmap) {
         ctx.drawImage(bitmap, 0, 0, offscreen.width, offscreen.height);
     }
+}
 
+window.addEventListener('mousemove', (e) => {
+    const idx2 = Math.floor((e.clientX / window.innerWidth) * bitmaps.length)
+
+    if (idx !== idx2) {
+        idx = idx2;
+        requestAnimationFrame(render)
+    }
+
+    e.preventDefault()
 })
+
+window.addEventListener('touchmove', (e) => {
+    const x = e.touches[0].clientX
+
+    const idx2 = Math.floor((x / window.innerWidth) * bitmaps.length)
+
+    if (idx !== idx2) {
+        idx = idx2;
+        requestAnimationFrame(render)
+    }
+
+
+    e.preventDefault()
+}, { passive: false })
+
 
 let decoder = new VideoDecoder({
     output: async frame => {
-        ctx.drawImage(frame, 0, 0, offscreen.width, offscreen.height);
+        // ctx.drawImage(frame, 0, 0, offscreen.width, offscreen.height);
 
-        frames.push(frame)
+        // frames.push(frame)
         bitmaps.push(await createImageBitmap(frame))
         // Close ASAP.
         frame.close();
 
+        requestAnimationFrame(render)
+
         // Draw some optional stats.
-        ctx.font = '35px sans-serif';
-        ctx.fillStyle = "#ffffff";
-        ctx.fillText(getFrameStats(), 40, 40, offscreen.width);
+        // ctx.font = '35px sans-serif';
+        // ctx.fillStyle = "#ffffff";
+        // ctx.fillText(getFrameStats(), 40, 40, offscreen.width);
     },
     error: e => console.error(e),
 });
