19b2a27b50c2e9756b19b44cdebf0b767ffcc240
Ben Foxall <benfoxall@gmail.com>
2022-02-20 16:47:04 +0000

    Use coded size

--- src/worker.js
+++ src/worker.js
@@ -4,10 +4,11 @@ self.onmessage = (e) => {
   const file = e.data;
 
   let demuxer = new MP4Demuxer(file);
+  let bitmapOpts = {};
 
   let decoder = new VideoDecoder({
     output: async (frame) => {
-      const bitmap = await createImageBitmap(frame);
+      const bitmap = await createImageBitmap(frame, bitmapOpts);
 
       frame.close();
 
@@ -17,6 +18,9 @@ self.onmessage = (e) => {
   });
 
   demuxer.getConfig().then((config) => {
+    bitmapOpts.resizeHeight = config.codedHeight;
+    bitmapOpts.resizeWidth = config.codedWidth;
+
     decoder.configure(config);
     demuxer.start((chunk) => {
       decoder.decode(chunk);
