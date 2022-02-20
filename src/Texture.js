5a69815b14210a2a83e9d1b5a03b7712090da8f4
Ben Foxall <benfoxall@gmail.com>
2022-02-20 20:01:00 +0000

    Clean up bits

--- src/Texture.js
+++ src/Texture.js
@@ -17,49 +17,38 @@ precision mediump float;
 precision mediump sampler3D;
 
 uniform vec2 resolution;
-uniform float time;
 
 uniform float u_slide;
+uniform sampler3D u_cubetex;
 
 out vec4 outColor;
 
-uniform sampler3D u_cubetex;
 
 void main() {
-  // vec2 uv = gl_FragCoord.xy / resolution;
-  // float color = 0.0;
-  // // lifted from glslsandbox.com
-  // color += sin( uv.x * cos( time / 3.0 ) * 60.0 ) + cos( uv.y * cos( time / 2.80 ) * 10.0 );
-  // color += sin( uv.y * sin( time / 2.0 ) * 40.0 ) + cos( uv.x * sin( time / 1.70 ) * 40.0 );
-  // color += sin( uv.x * sin( time / 1.0 ) * 10.0 ) + sin( uv.y * sin( time / 3.50 ) * 80.0 );
-  // color *= sin( time / 10.0 ) * 0.5;
+  vec2 point = gl_FragCoord.xy / resolution.xy;
 
-  // gl_FragColor = vec4( vec3( color * 0.5, sin( color + time / 2.5 ) * 0.75, color ), 1.0 );
-
-  outColor = vec4(gl_FragCoord.x/100.0, gl_FragCoord.y/100.0, 1.0, 1.0);
-
-  outColor = texture(u_cubetex, vec3(1.0,1.0,1.0));
+  float x = point.x;
+  float y = point.y;
 
   outColor = texture(u_cubetex, 
     vec3(
-      (gl_FragCoord.x/100.0), 
-      gl_FragCoord.y/100.0,
-      u_slide
+      point, 
+      u_slide * x * y
     )
   );
-
 }
 `;
 
-export class Cube extends ViewerBase {
+export class Texture extends ViewerBase {
   bitmaps = [];
   canvas = document.createElement("canvas");
-  //   ctx = this.canvas.getContext("2d");
 
   constructor(url) {
     super(url);
 
     document.body.appendChild(this.canvas);
+
+    this.canvas.width = this.canvas.height = 512;
 
     const gl = (this.gl = twgl.getContext(this.canvas));
     const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
@@ -81,10 +70,17 @@ export class Cube extends ViewerBase {
       height: 512,
       depth: 512,
       wrap: gl.CLAMP_TO_EDGE,
-      minMag: gl.NEAREST,
+      minMag: gl.LINEAR, //gl.NEAREST
     }));
 
     gl.bindTexture(gl.TEXTURE_3D, texture);
+
+    const ext = gl.getExtension("GMAN_webgl_memory");
+    // ...
+    if (ext) {
+      const info = ext.getMemoryInfo();
+      console.log(info);
+    }
 
     function render(time) {
       // twgl.resizeCanvasToDisplaySize(gl.canvas);
@@ -112,47 +108,27 @@ export class Cube extends ViewerBase {
     }
     requestAnimationFrame(render);
 
-    // document.body.appendChild(this.canvas);
-
-    // this.render = memo(raf(this.render.bind(this)));
-
-    // window.addEventListener("scroll", () => {
-    //   const idx = Math.floor(window.scrollY / scrollStep);
-
-    //   this.render(this.bitmaps[idx]);
-    // });
-
     this.z = 0;
   }
 
   handle(bitmap) {
-    // gl.texImage3D(gl.TEXTURE_3D, level, internalFormat,
-    // srcFormat, srcType, image);
-
     const z = this.z++ % 512;
 
-    // temp
     if (this.z > 512) return;
 
     const { gl } = this;
 
     gl.bindTexture(gl.TEXTURE_3D, this.texture);
 
-    // console.log(z);
-
     gl.texSubImage3D(
       gl.TEXTURE_3D,
       0,
-      0, //x
-      0, //y
-      // 0,//z
-      z,
-
-      // bitmap.width,
-      // bitmap.height,
-      512,
-      512,
-      1,
+      0, // x
+      0, // y
+      z, // z
+      512, // width
+      512, // height
+      1, // depth
       gl.RGBA,
       gl.UNSIGNED_BYTE,
       bitmap
