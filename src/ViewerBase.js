d01218dd52a14d24ab3acd33ab358cc1a3e23be0
Ben Foxall <benfoxall@gmail.com>
2022-02-20 11:50:21 +0000

    Prettier

--- src/ViewerBase.js
+++ src/ViewerBase.js
@@ -1,21 +1,20 @@
 const workerSrc = new URL("./worker.js", import.meta.url).href;
-console.log(workerSrc)
+console.log(workerSrc);
 
 export class ViewerBase {
-    constructor(src) {
+  constructor(src) {
+    const decoder = new Worker(workerSrc, { type: "module" });
 
-        const decoder = new Worker(workerSrc, { type: "module" })
+    decoder.addEventListener("message", (event) => {
+      if (event.data instanceof ImageBitmap) {
+        this.handle(event.data);
+      } else {
+        console.error("unexpected");
+      }
+    });
 
-        decoder.addEventListener('message', (event) => {
-            if (event.data instanceof ImageBitmap) {
-                this.handle(event.data);
-            } else {
-                console.error("unexpected")
-            }
-        })
+    decoder.postMessage(src);
+  }
 
-        decoder.postMessage(src)
-    }
-
-    handle(bitmap) { }
+  handle(bitmap) {}
 }
--- src/worker.js
+++ src/worker.js
@@ -1,26 +1,25 @@
-
-import { MP4Demuxer } from './mp4/MP4Demuxer.js'
+import { MP4Demuxer } from "./mp4/MP4Demuxer.js";
 
 self.onmessage = (e) => {
-    const file = e.data;
+  const file = e.data;
 
-    let demuxer = new MP4Demuxer(file);
+  let demuxer = new MP4Demuxer(file);
 
+  let decoder = new VideoDecoder({
+    output: async (frame) => {
+      const bitmap = await createImageBitmap(frame);
 
-    let decoder = new VideoDecoder({
-        output: async frame => {
-            const bitmap = await createImageBitmap(frame)
+      frame.close();
 
-            frame.close();
+      self.postMessage(bitmap, [bitmap]);
+    },
+    error: (e) => console.error(e),
+  });
 
-            self.postMessage(bitmap, [bitmap])
-
-        },
-        error: e => console.error(e),
+  demuxer.getConfig().then((config) => {
+    decoder.configure(config);
+    demuxer.start((chunk) => {
+      decoder.decode(chunk);
     });
-
-    demuxer.getConfig().then((config) => {
-        decoder.configure(config);
-        demuxer.start((chunk) => { decoder.decode(chunk); })
-    });
-}
+  });
+};
