cb940655c00e532e6e4edaa05e6210ffb7d594b3
Ben Foxall <benfoxall@gmail.com>
2022-02-27 12:59:34 +0000

    Copy in Three sample code

--- src/Cube.js
+++ src/Cube.js
@@ -1,43 +1,8 @@
 import { ViewerBase } from "./ViewerBase.js";
-import * as twgl from "https://cdn.skypack.dev/twgl.js";
+
+import * as THREE from "https://cdn.skypack.dev/three@0.138.0";
 
 const scrollStep = 12;
-
-const vs = `
-#version 300 es
-in vec4 position;
-
-void main() {
-  gl_Position = position;
-}
-`;
-const fs = `
-#version 300 es
-precision mediump float;
-precision mediump sampler3D;
-
-uniform vec2 resolution;
-
-uniform float u_slide;
-uniform sampler3D u_cubetex;
-
-out vec4 outColor;
-
-
-void main() {
-  vec2 point = gl_FragCoord.xy / resolution.xy;
-
-  float x = point.x;
-  float y = point.y;
-
-  outColor = texture(u_cubetex, 
-    vec3(
-      point, 
-      u_slide * x * y
-    )
-  );
-}
-`;
 
 export class Cube extends ViewerBase {
   bitmaps = [];
@@ -48,91 +13,59 @@ export class Cube extends ViewerBase {
 
     document.body.appendChild(this.canvas);
 
-    this.canvas.width = this.canvas.height = 512;
+    const rect = this.canvas.getBoundingClientRect();
 
-    const gl = (this.gl = twgl.getContext(this.canvas));
-    const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
+    const width = rect.width; //window.innerWidth * 0.8;
+    const height = rect.height; //window.innerHeight * 0.8;
 
-    console.log("using: " + gl.getParameter(gl.VERSION)); // eslint-disable-line
-    if (!twgl.isWebGL2(gl)) {
-      alert("Sorry, this example requires WebGL 2.0"); // eslint-disable-line
-      return;
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+
+    const renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+    renderer.setSize(width, height);
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    function animate() {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
     }
 
-    const arrays = {
-      position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
-    };
-    const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
-
-    const texture = (this.texture = twgl.createTexture(gl, {
-      target: gl.TEXTURE_3D,
-      width: 512,
-      height: 512,
-      depth: 512,
-      wrap: gl.CLAMP_TO_EDGE,
-      minMag: gl.LINEAR, //gl.NEAREST
-    }));
-
-    gl.bindTexture(gl.TEXTURE_3D, texture);
-
-    const ext = gl.getExtension("GMAN_webgl_memory");
-    // ...
-    if (ext) {
-      const info = ext.getMemoryInfo();
-      console.log(info);
-    }
-
-    function render(time) {
-      // twgl.resizeCanvasToDisplaySize(gl.canvas);
-      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-      const uniforms = {
-        time: time * 0.001,
-        resolution: [gl.canvas.width, gl.canvas.height],
-        u_cubetex: texture,
-        u_slide: slide.valueAsNumber / 512,
-      };
-
-      window.uu = uniforms;
-
-      gl.useProgram(programInfo.program);
-      twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-      twgl.setUniforms(programInfo, uniforms);
-      // gl.activeTexture(gl.TEXTURE0);
-      // twgl.setTexture(texture);
-      // gl.bindTexture(gl.TEXTURE_3D, texture);
-      window.twgl = twgl;
-      twgl.drawBufferInfo(gl, bufferInfo);
-
-      requestAnimationFrame(render);
-    }
-    requestAnimationFrame(render);
-
-    this.z = 0;
+    animate();
   }
 
   handle(bitmap) {
-    const z = this.z++ % 512;
+    // const z = this.z++ % 512;
 
-    if (this.z > 512) return;
+    // if (this.z > 512) return;
 
-    const { gl } = this;
+    // const { gl } = this;
 
-    gl.bindTexture(gl.TEXTURE_3D, this.texture);
+    // gl.bindTexture(gl.TEXTURE_3D, this.texture);
 
-    gl.texSubImage3D(
-      gl.TEXTURE_3D,
-      0,
-      -600, // x
-      -600, // y
-      z, // z
-      512, // width
-      512, // height
-      1, // depth
-      gl.RGBA,
-      gl.UNSIGNED_BYTE,
-      bitmap
-    );
+    // gl.texSubImage3D(
+    //   gl.TEXTURE_3D,
+    //   0,
+    //   -600, // x
+    //   -600, // y
+    //   z, // z
+    //   512, // width
+    //   512, // height
+    //   1, // depth
+    //   gl.RGBA,
+    //   gl.UNSIGNED_BYTE,
+    //   bitmap
+    // );
 
     bitmap.close();
   }
